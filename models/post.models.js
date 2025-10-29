ef157123482ef3d1234008d8cce3357b3b118f5c
devendragautam <gautamdevendra963@gmail.com>
2025-10-29 13:15:08 +0530

    market model added

---
+++ models/post.models.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const PostSchema = new mongoose.Schema(
+  {
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 1000, 
+    },
+    image: {
+      type: String, 
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User", 
+      },
+    ],
+    comments: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+        text: {
+          type: String,
+          trim: true,
+        },
+        createdAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const Post = mongoose.model("Post", PostSchema);
