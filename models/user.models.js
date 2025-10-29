7bde845fb175ff561f777f1e8f15b163ac5129ee
devendragautam <gautamdevendra963@gmail.com>
2025-10-29 12:40:20 +0530

    basic model setup

---
+++ models/user.models.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    chats: [
+      {
+        date: {
+          type: String,
+        },
+        time: {
+          type: String,
+        },
+        partner: {
+          type: mongoose.Schema.Types.ObjectId, 
+          ref: "User",
+        },
+        messages: [ 
+          {
+            sender: {
+              type: String,
+            },
+            receiver: { 
+              type: String,
+            }
+          },
+        ],
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", UserSchema);
